94c63766c42989c7619984f1bdc57eab2ecc08c4
JustinlikesNuggets <116641616+JustinlikesNuggets@users.noreply.github.com>
2025-04-24 17:02:53 +0800

    Update share_page1.py

--- share_page1.py
+++ share_page1.py
@@ -1,19 +1,18 @@
-from flask import Flask, render_template, request 
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-# Dummy data to be replaced with real ones from DB later 
 @app.route("/share")
 def share_page():
+    # List of people and their image filenames
     users = [
         ("James", "images/avatar1.png"),
         ("Justin", "images/avatar2.png"),
         ("Sacha", "images/avatar3.png"),
-        ("Rishon", "images/avatar4.png"),
+        ("Rishon", "images/avatar4.png")
     ]
     return render_template("share.html", users=users)
 
-# This handles the form submission
 @app.route("/share-data", methods=["POST"])
 def handle_share():
     selected = request.form.getlist("share_to")
