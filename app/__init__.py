fe0581c9a3205e5ef7bca378602702396f26d8a8
Rishon Jose <rishonjose@gmail.com>
2025-05-05 04:19:55 +0800

    Added SQLAlchemy BillEntry model and auto-migrations for database storage

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,17 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate  import Migrate
+
+db      = SQLAlchemy()
+migrate = Migrate()
 
 application = Flask(__name__)
 application.config['SECRET_KEY'] = 'amber_pearl_latte_is_the_best'
+
+application.config['SQLALCHEMY_DATABASE_URI']  = 'sqlite:///app.db'
+application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(application)
+migrate.init_app(application, db)
 
 from app import routes
