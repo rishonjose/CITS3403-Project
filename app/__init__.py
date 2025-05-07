4196ba6897ef59fe64bd12dbdcc19206c431a967
SachaPoulet <sachapoulet05@gmail.com>
2025-05-07 20:45:51 +0800

    Modifying __init__.py Adding import of python-dotenv, which uses load_dotenv() to grab credentials from .env file. Also added another application.config for local testing. register_blueprint() at the bottom links google login and authentication with the /login route. Commit falls under issue #26

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,7 @@
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate  import Migrate
@@ -12,6 +16,7 @@ application.config['SECRET_KEY'] = 'amber_pearl_latte_is_the_best'
 
 application.config['SQLALCHEMY_DATABASE_URI']  = 'sqlite:///app.db'
 application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+application.config["OAUTHLIB_INSECURE_TRANSPORT"] = True  # only for local testing
 
 db.init_app(application)
 migrate.init_app(application, db)
@@ -26,3 +31,6 @@ def load_user(user_id):
     return User.query.get(int(user_id))
 
 from app import routes
+from app.auth import auth_bp, google_bp
+application.register_blueprint(google_bp, url_prefix="/login")
+application.register_blueprint(auth_bp)
