7e5f7052c63ce25deb91ba6d03a33f7ef2bd1c76
JustinlikesNuggets <116641616+JustinlikesNuggets@users.noreply.github.com>
2025-05-10 05:49:52 +0800

    Update share_page.py

--- share_page.py
+++ share_page.py
@@ -2,18 +2,14 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/share", methods=["GET", "POST"])
+@app.route("/share")
 def share_page():
+    # List of people and their image filenames
     users = [
         ("James", "images/avatar1.png"),
         ("Justin", "images/avatar2.png"),
         ("Sacha", "images/avatar3.png"),
         ("Rishon", "images/avatar4.png")
     ]
-
-    shared_to = []
-    if request.method == "POST":
-        shared_to = request.form.getlist("share_to")
-        print("✅ Shared with:", shared_to)
-
-    return render_template("share.html", users=users, shared_to=shared_to)
+    
+    return render_template("share.html", users=users, share_to=selected)
