c1b196dace7c6d675b4db0fb74cbf9a84456f358
JustinlikesNuggets <116641616+JustinlikesNuggets@users.noreply.github.com>
2025-05-11 07:32:55 +0800

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -9,16 +9,14 @@ login_manager = LoginManager()
 
 application = Flask(__name__)
 application.config['SECRET_KEY'] = 'amber_pearl_latte_is_the_best'
-
 application.config['SQLALCHEMY_DATABASE_URI']  = 'sqlite:///app.db'
 application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(application)
-migrate.init_app(application, db)
+migrate.init_app(application, db) 
 login_manager.init_app(application)              
-login_manager.login_view = 'login'               # redirect anonymous users here
+login_manager.login_view = 'login'
 
-# ensure User model is imported so the loader can find it
 from app.models import User                       
 
 @login_manager.user_loader
