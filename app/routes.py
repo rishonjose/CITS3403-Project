3fcc305f3a3d1a451690d2817e3bddd273afe693
SachaPoulet <sachapoulet05@gmail.com>
2025-05-13 16:53:05 +0800

    Add CSRFProtect import from flask_wtf Solves undefined problem of intialise CSRF protection line (CSRFProtect was not imported before)

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 
 from flask import Flask, render_template, request, flash, redirect, url_for
+from flask_wtf.csrf import CSRFProtect
 from app import application, db
 from app.models import BillEntry, User
 from datetime import date 
