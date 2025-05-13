9a0443d9931b67c28b86664c0c41c782398dc142
SachaPoulet <sachapoulet05@gmail.com>
2025-05-14 03:56:04 +0800

    Adding login_manager from flask_wtf to track when users are in session Also added CSRFPRotect in __init__.py. Collective issues #20 and #26

--- app/__init__.py
+++ app/__init__.py
@@ -11,12 +11,23 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate  import Migrate
 from flask_login import LoginManager
 
-db      = SQLAlchemy()
-migrate = Migrate()
-login_manager = LoginManager()
+class Config():
+    SECRET_KEY = os.environ.get("SECRET_KEY")
 
 application = Flask(__name__)
-application.config['SECRET_KEY'] = 'amber_pearl_latte_is_the_best'
+application.config.from_object(Config)
+
+from sqlalchemy.orm import DeclarativeBase
+
+class Base(DeclarativeBase):
+    pass
+
+db = SQLAlchemy(model_class=Base, session_options={"autoflush": False})
+
+migrate = Migrate()
+login = LoginManager()
+login.login_view = 'login'             # login view function
+login.session_protection = 'strong'     # session protection level
 
 application.config['SQLALCHEMY_DATABASE_URI']  = 'sqlite:///app.db'
 application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -24,8 +35,9 @@ application.config["OAUTHLIB_INSECURE_TRANSPORT"] = True  # only for local testi
 
 db.init_app(application)
 migrate.init_app(application, db)
-login_manager.init_app(application)   
+login.init_app(application)   
 oauth.init_app(application)  
+
 google = oauth.register(
     name='google',
     api_base_url='https://www.googleapis.com/oauth2/v3/',
@@ -35,16 +47,20 @@ google = oauth.register(
     authorize_url='https://accounts.google.com/o/oauth2/auth',
     client_kwargs={'scope': 'openid email profile'},
 )
-login_manager.session_protection = 'strong'  # session protection level
-login_manager.login_view = 'login'               # redirect anonymous users here
 
 # ensure User model is imported so the loader can find it
 from app.models import User                       
 
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
 
 from app import routes
+from flask_wtf.csrf import CSRFProtect
+csrf = CSRFProtect(application)
+
 from app.auth import auth_bp
 application.register_blueprint(auth_bp)
+
+@login.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))  # This function loads the user from the session
+
+from app import forms
--- run.py
+++ run.py
@@ -1,11 +1,14 @@
 from app import application
 from flask_migrate import upgrade
+from flask_wtf import LoginManager
+
+login_manager = LoginManager()
 
 if __name__ == "__main__":
     # Ensure migrations are applied before the first request
     with application.app_context():
         # This will create the migrations folder if missing, and then run all unapplied migrations
         upgrade()
-    
+    login_manager.login_view = "login"
     # Run the application
     application.run(debug=True)
