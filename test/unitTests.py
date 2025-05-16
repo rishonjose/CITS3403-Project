3d78aaa2ea37a446ddbaf78458ca7d85f32cb1b8
James Felstead <23962355@student.uwa.edu.au>
2025-05-17 01:12:24 +0800

    implemented create ap into unittests function

--- test/unitTests.py
+++ test/unitTests.py
@@ -1,19 +1,22 @@
 import unittest
 import datetime
-from app import application, db
+from app import db, create_app
 from app.models import User, BillEntry
 from app.config import TestingConfig
 from sqlalchemy.exc import IntegrityError
 
 class UserTests(unittest.TestCase):
     def setUp(self):
-        application.config.from_object(TestingConfig)
-        self.app_ctx = application.app_context()
+        # Create app with TestingConfig
+        app = create_app(TestingConfig)
+        self.app_ctx = app.app_context()
         self.app_ctx.push()
+
+        # Create all tables
         db.create_all()
 
     def test_register_user(self):
-        user = User(username='test',email="test@test",profile_pic='default.png')
+        user = User(username='test', email="test@test", profile_pic='default.png')
         user.set_password('pass')
         db.session.add(user)
         db.session.commit()
@@ -27,19 +30,19 @@ class UserTests(unittest.TestCase):
         self.assertFalse(user.check_password('wrongpassword'))
 
     def test_unique_username_and_email(self):
-        user1 = User(username='unique', email='unique@test.com',profile_pic='default.png')
+        user1 = User(username='unique', email='unique@test.com', profile_pic='default.png')
         user1.set_password('pass')
         db.session.add(user1)
         db.session.commit()
 
-        user2 = User(username='unique', email='unique2@test.com',profile_pic='default.png')
+        user2 = User(username='unique', email='unique2@test.com', profile_pic='default.png')
         user2.set_password('pass')
         db.session.add(user2)
         with self.assertRaises(IntegrityError):
             db.session.commit()
         db.session.rollback()
 
-        user3 = User(username='unique3', email='unique@test.com',profile_pic='default.png')
+        user3 = User(username='unique3', email='unique@test.com', profile_pic='default.png')
         user3.set_password('pass')
         db.session.add(user3)
         with self.assertRaises(IntegrityError):
@@ -47,7 +50,7 @@ class UserTests(unittest.TestCase):
         db.session.rollback()
 
     def test_user_bill_entry_relationship(self):
-        user = User(username='billuser', email='bill@test.com',profile_pic='default.png')
+        user = User(username='billuser', email='bill@test.com', profile_pic='default.png')
         user.set_password('pass')
         db.session.add(user)
         db.session.commit()
@@ -65,7 +68,7 @@ class UserTests(unittest.TestCase):
 
         self.assertEqual(user.entries.count(), 1)
         self.assertEqual(user.entries.first().category, 'electricity')
-
+    
     def tearDown(self):
         db.session.remove()
         db.drop_all()
