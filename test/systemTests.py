bc55008526b9b945b02a1e8028451b6fa1e53f6d
James Felstead <23962355@student.uwa.edu.au>
2025-05-17 01:24:41 +0800

    implemented create ap into systemTests function

--- test/systemTests.py
+++ test/systemTests.py
@@ -1,26 +1,32 @@
 import unittest
-from app import application, db
+from app import db, create_app
 from app.models import User
 from app.config import TestingConfig
 
 class SystemTestCase(unittest.TestCase):
     def setUp(self):
-        application.config.from_object(TestingConfig)
-        self.app = application.test_client()
-        self.app_ctx = application.app_context()
+        # Create app with testing config
+        self.app = create_app(TestingConfig)
+        self.app_ctx = self.app.app_context()
         self.app_ctx.push()
+
+        # Set up test client
+        self.client = self.app.test_client()
+
+        # Set up database
         db.create_all()
 
+        # Add a test user
         user = User(username='tester', email='tester@example.com', profile_pic='default.png')
         user.set_password('password123')
         db.session.add(user)
         db.session.commit()
 
-    def test_homepage_access(self):
-        response = self.app.get('/')
-        self.assertEqual(response.status_code, 200)
-
     def tearDown(self):
         db.session.remove()
         db.drop_all()
         self.app_ctx.pop()
+
+    def test_homepage_access(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
